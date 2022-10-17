b625b2e67386b6ff4c8aa291132e37606043cadd
Nate Jackson <11004247@live.mercer.edu>
2022-10-17 15:05:28 -0400

    touch up lisp comments

--- lisp/diffusion.lisp
+++ lisp/diffusion.lisp
@@ -1,10 +1,14 @@
 #!/usr/bin/sbcl --script
+;;;; CSC330 Project 2 F22
+;;;; Written by Nate Jackson
+;;;; Modeling the diffusion of gas within a room
 
 ;; (declaim (sb-ext:muffle-conditions sb-ext:compiler-note))
 
 (progn
-(defconstant maxsize 10)
-(defvar cube (make-array '(10 10 10)))
+(defconstant maxsize 10) ;const for room size
+(defvar cube (make-array '(10 10 10))) ;the room
+;;zeroing out the room
 (loop for i from 0 to (1- maxsize)
     do (loop for j from 0 to (1- maxsize)
         do (loop for k from 0 to (1- maxsize)
@@ -19,10 +23,11 @@
 (setf distance_between_blocks (/ room_dimension maxsize))
 (defvar DTerm  )
 (setf DTerm (* diffusion_coefficient (/ timestep (* distance_between_blocks distance_between_blocks))))
+;;first cell
 (setf (aref cube 0 0 0) 1.0e21)
 
-(defvar pass 0)
-(defvar mytime)
+(defvar pass 0) ;conservation of mass
+(defvar mytime) ;system time
 (setq mytime 0.0)
 (defvar myratio 0.0)
 
@@ -67,6 +72,8 @@
             (setq sumval (+ sumval (aref cube i j k))))))
             
 (setf myratio (/ minval maxval))
+
+;;output
 (format t "~f ~f" mytime (aref cube 0 0 0))
 (format t " ~f"(aref cube (1- maxsize) 0 0))
 (format t " ~f"(aref cube (1- maxsize) (1- maxsize) 0))
